db90cf23827027e7a62a7080e95ccb4440df877c
shash29exe <zzxc67643@gmail.com>
2025-11-04 12:13:00 +0500

    Google sheets settings.

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -1,3 +1,18 @@
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+
+from config import GOOGLE_CREDENTIALS_PATH
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+with open(GOOGLE_CREDENTIALS_PATH, 'r') as f:
+    creds_dict = json.load(f)
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+client = gspread.authorize(credentials)
+
+SPREADSHEET_NAME = "TaxiService"
+
+spreadsheet = client.open(SPREADSHEET_NAME)
+sheet = spreadsheet.sheet1
